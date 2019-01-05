2903b9586d364f5b669d613063ea25e828e3e687
bntwng <boranti1995@gmail.com>
2019-01-06 05:53:47 +0900

    2分法を追加

---
+++ src/Binary.kt
@@ -0,0 +1,5 @@
+import kotlin.math.cos
+import kotlin.math.sin
+
+//piを求めるために使う式
+fun binaryF(x: Double): Double = sin(x/4.0) - cos(x/4.0)
--- src/IterationMethods.kt
+++ src/IterationMethods.kt
@@ -1,4 +1,6 @@
 import java.lang.RuntimeException
+import kotlin.math.abs
+import kotlin.math.pow
 
 //反復法の本体
 tailrec fun<T> iteration(
@@ -16,15 +18,39 @@ tailrec fun<T> iteration(
         return xNew
     }
     //反復回数判定
-    if(n > nMax){
-        println("収束しませんでした")
-        throw RuntimeException()
-    }
+    if(n > nMax) throw RuntimeException("収束しませんでした")
     //末尾再帰
     return iteration(f, xNew, cond, nMax, n+1)
 }
 
+//二分法の本体
+tailrec fun binaryIteration(
+    f: (Double) -> Double, //入力関数
+    a: Double,
+    b: Double,
+    cond: (Double) -> Boolean, //判定関数
+    nMax: Int = 10000, //反復回数上限
+    n: Int = 1 //反復回数
+): Double {
+    val c = (a + b) / 2.0
+    if(cond(c)){
+        println("反復回数: $n, ans: $c")
+        return c
+    }
+    //反復回数判定
+    if(n > nMax) throw RuntimeException("収束しませんでした。ans: $c")
+    return when {
+        f(a) * f(c) > 0 -> binaryIteration(f, c, b, cond, nMax, n+1)
+        f(b)*f(c) > 0 -> binaryIteration(f, a, c, cond, nMax, n+1)
+        else -> throw RuntimeException("条件判定に失敗しました")
+    }
+}
+
 fun main(args: Array<String>) {
-    var ans = iteration(::newtonF, 10000000000000000000.0, ::condDouble)
-    println(ans * ans * ans) //解は2^(1/3)になる
+    val newtonAns = iteration(::newtonF, 10000000000000000000.0, ::condDouble)
+    println(newtonAns * newtonAns * newtonAns) //解は2^(1/3)になる
+    //(恐らくsin/cosの精度が原因で)Float.MIN_VALUEの精度が出なかったので、Wikipediaの記載から10^(-15)を採用
+    val binaryAns =
+        binaryIteration(::binaryF, 0.0, 5.0, { abs(binaryF(it)) < 10.0.pow(-15) })
+    println(binaryAns)
 }
