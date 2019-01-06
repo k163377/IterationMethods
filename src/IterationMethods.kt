2d2520bbe020eca1da735813a5f214c0ff5efbe4
bntwng <boranti1995@gmail.com>
2019-01-06 13:21:54 +0900

    簡易ニュートン法追加

--- src/IterationMethods.kt
+++ src/IterationMethods.kt
@@ -50,8 +50,11 @@ tailrec fun binaryIteration(
 }
 
 fun main(args: Array<String>) {
-    val newtonAns = iteration(::newtonF, 10000000000000000000.0, ::condDouble)
+    val newtonAns = iteration(::newtonF, 1.0, ::condDouble)
     println(newtonAns * newtonAns * newtonAns) //解は2^(1/3)になる
+
+    val easyNewtonAns = iteration(::easyNewtonF, 1.0, ::condDouble)
+    println(easyNewtonAns * easyNewtonAns * easyNewtonAns)
 
     val binaryAns = binaryIteration(::binaryF, 0.0, 0.0, 5.0)
     println(binaryAns)
--- src/Newton.kt
+++ src/Newton.kt
@@ -5,3 +5,6 @@ fun condDouble(x: Double, xNew: Double): Boolean = abs(x - xNew) < Float.MIN_VAL
 
 //ニュートン法でf(x) = x^3 - 2.0のx=0における実数解を求めるための漸化式
 fun newtonF(x: Double): Double = x - ((x*x*x - 2.0) / (3.0*x*x))
+
+//簡易ニュートン法、x0=1.0
+fun easyNewtonF(x: Double): Double = x - ((x*x*x - 2.0) / 3.0)
