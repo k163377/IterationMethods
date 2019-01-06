191c604c7c3f1f7a7138b92de902232387e3223f
bntwng <boranti1995@gmail.com>
2019-01-06 18:49:22 +0900

    シーケンスを使った二分法の実装を追加

--- src/main/Sequence.kt
+++ src/main/Sequence.kt
@@ -1,5 +1,9 @@
 package main
 
+import java.lang.RuntimeException
+import kotlin.math.abs
+
+//反復法
 fun<T : Any> iterationSequence(
     f: (T) -> T, //漸化式
     x0: T, //初期値
@@ -11,4 +15,25 @@ fun<T : Any> iterationSequence(
     if(x == null) true else !cond(x, xNew) //条件判定、xは初期値nullなのでnull回避が必要
 }.map {
     it.second //Pairから配列へ変換
+}.take(nMax).toList() //nMaxまで行ったら終了、Listにして返却
+
+//二分法の中点計算用
+fun calcHalf(a: Double, b: Double): Double = (a + b) / 2.0
+//二分法
+fun binaryIterationSequence(
+    f: (Double) -> Double, //入力関数
+    trueValue: Double, //二分法の真値
+    a0: Double, //小さい方の初期値
+    b0: Double, //大きい方の初期値
+    nMax: Int = 10000 //反復回数上限
+): List<Double> = generateSequence(Triple(a0, b0, calcHalf(a0, b0))) { (a, b, c) ->
+    when { //更新
+        f(a) * f(c) > 0.0 -> Triple(c, b, calcHalf(c, b))
+        f(b) * f(c) > 0.0 -> Triple(a, c,  calcHalf(a, c))
+        else -> throw RuntimeException("条件判定に失敗しました")
+    }
+}.map {
+    it.third //Tripleから配列へ変換
+}.takeFor {
+    abs(f(it) - trueValue) >= 1.0E-15
 }.take(nMax).toList() //nMaxまで行ったら終了、Listにして返却
--- src/test/SequenceTest.kt
+++ src/test/SequenceTest.kt
@@ -1,16 +1,23 @@
 package test
 
-import main.condDouble
-import main.iterationSequence
-import main.newtonF
+import main.*
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import kotlin.math.PI
 
 class SequenceTest{
     @Test
     fun testIterationSequence(){
         val ans = iterationSequence(::newtonF, 1.0, ::condDouble)
         val last = ans.last()
+        Assertions.assertTrue(10000 > ans.size)
         Assertions.assertEquals(2.0, last * last * last, Float.MIN_VALUE.toDouble())
+    }
+
+    @Test
+    fun testBinarySequence(){
+        val ans = binaryIterationSequence(::binaryF, 0.0, 0.0, 5.0)
+        Assertions.assertTrue(10000 > ans.size)
+        Assertions.assertEquals(PI, ans.last(), 1.0E-15)
     }
 }
