a735eb33da7ab356ad97ad6b568195946cffadf0
wrongwrongq <boranti1995@gmail.com>
2019-01-12 14:58:17 +0900

    側を追加

---
+++ src/main/StreamIteration.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.UnaryOperator;
+
+public class StreamIteration {
+    public <T> List<T> streamIteration(
+            UnaryOperator<T> f,
+            T x0,
+            BiPredicate<T, T> cond,
+            int nMax
+    ) {
+
+    }
+}
